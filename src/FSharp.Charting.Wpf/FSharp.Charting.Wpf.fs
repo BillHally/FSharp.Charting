18db71ee0cd9b7b9d9d28ce4f76443fdff6f2c30
BillHally <bill.hally@gmail.com>
2016-06-27 16:16:45 +0100

    Avoids the compiler warning about recursive objects (rather than turn it off for the whole file)

--- src/FSharp.Charting.Wpf/FSharp.Charting.Wpf.fs
+++ src/FSharp.Charting.Wpf/FSharp.Charting.Wpf.fs
@@ -718,16 +718,25 @@ namespace FSharp.Charting
                 series.ItemsSource <- data
                 let chart = GenericChart(model)
                 match data with 
-                | :? INotifyCollectionChanged as i -> 
-                      let rec handler = NotifyCollectionChangedEventHandler(fun _ _ -> 
-                        series.ItemsSource <- data
-                        match model.PlotView with 
-                        | null -> ()
-                        | _ ->  
-                           // An exception will indicate form is no longer working, e.g. shutdown or disposed, so disconnect
-                           try model.InvalidatePlot(true) 
-                           with _ -> i.CollectionChanged.RemoveHandler handler)
-                      i.CollectionChanged.AddHandler handler
+                | :? INotifyCollectionChanged as i ->
+                    let handlerRef = ref None
+                    let handler =
+                        NotifyCollectionChangedEventHandler
+                            (
+                                fun _ _ -> 
+                                    series.ItemsSource <- data
+                                    match model.PlotView with 
+                                    | null -> ()
+                                    | _ ->  
+                                        // An exception will indicate form is no longer working, e.g. shutdown or disposed, so disconnect
+                                        try model.InvalidatePlot(true) 
+                                        with _ ->
+                                            match !handlerRef with
+                                            | Some handler -> i.CollectionChanged.RemoveHandler handler
+                                            | None -> ()
+                            )
+                    handlerRef := Some handler
+                    i.CollectionChanged.AddHandler handler
                 | _ -> ()
                 chart
 
